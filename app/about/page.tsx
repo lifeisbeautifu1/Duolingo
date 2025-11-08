b460ec5cdd477ef31ce4a3fd435576f2704cfcbb
Alexey Poltoradnev <poltoradnevone@gmail.com>
2025-11-08 17:28:48 +0400

    feat: updated favicon (#11)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-full w-full flex flex-col items-center justify-center">
-      <h1 className="font-bold text-xl">Home page</h1>
-      <Link href="/about" className="text-m text-blue-400">
-        About page
+      <h1 className="font-bold text-xl">About page</h1>
+      <Link href="/" className="text-m text-blue-400">
+        Home page
       </Link>
     </div>
   );
